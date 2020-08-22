bc0cd56e1fcd6c7ebf63bb58de40e621a5ba0759
zqbinary@foxmail.com <zqbinary@foxmail.com>
2020-08-22 11:22:36 +0800

    <code>init

---
+++ server/routes/index.js
@@ -0,0 +1,11 @@
+// routes/index.js and users.js
+import express from 'express';
+var router = express.Router();
+// ..stuff below
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+export default router;
---
+++ server/routes/users.js
@@ -0,0 +1,12 @@
+// routes/index.js and users.js
+import express from 'express';
+var router = express.Router();
+// ..stuff below
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+export default router;
